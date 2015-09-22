63df00d579fc577ea2c3b42525dcf81708cf2d8e
jshi17 <weiqian.shi@gmail.com>
2015-09-22 19:37:24 -0400

    chronometer setup

--- app/src/main/java/com/example/alexzheng/shijian/RecordTime.java
+++ app/src/main/java/com/example/alexzheng/shijian/RecordTime.java
@@ -1,17 +1,20 @@
 package com.example.alexzheng.shijian;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.TextView;
 
 public class RecordTime extends AppCompatActivity {
 
     private TextView mTimeTextView;
     private Button mStartButton, mResetButton, mSaveButton;
+    private Chronometer mChronometer;
 
     private boolean isStopped; // true if timer stopped, false if timer going
 
@@ -23,6 +26,7 @@ public class RecordTime extends AppCompatActivity {
         mTimeTextView = (TextView) findViewById(R.id.time_textView);
 
         setUpButtons();
+        setUpChronometer();
     }
 
     private void setUpButtons() {
@@ -48,6 +52,27 @@ public class RecordTime extends AppCompatActivity {
             public void onClick(View v) {
             }
         });
+    }
+
+    private void setUpChronometer() {
+        mChronometer = (Chronometer) findViewById(R.id.time_Chronometer);
+
+        // http://stackoverflow.com/questions/4152569/how-to-change-format-of-chronometer
+        mChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener(){
+            @Override
+            public void onChronometerTick(Chronometer cArg) {
+                long time = SystemClock.elapsedRealtime() - cArg.getBase();
+                int h = (int)(time/3600000);
+                int m = (int)(time - h*3600000)/60000;
+                int s = (int)(time - h*3600000- m*60000)/1000 ;
+                String hh = h < 10 ? "0"+h: h+"";
+                String mm = m < 10 ? "0"+m: m+"";
+                String ss = s < 10 ? "0"+s: s+"";
+                cArg.setText(hh+":"+mm+":"+ss);
+            }
+        });
+        mChronometer.setBase(SystemClock.elapsedRealtime());
+        mChronometer.start();
     }
 
     private void changeTimerState() {
